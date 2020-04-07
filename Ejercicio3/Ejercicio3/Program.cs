4a490080ed501a082643df41ca8239c067aff55f
resposito1620 <62219245+resposito1620@users.noreply.github.com>
2020-04-07 19:18:39 -0300

    Add files via upload

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba una palabra y sabrá si es palíndroma :) ");
+            String palabra = Console.ReadLine();
+            if (esPalindroma(palabra)) Console.WriteLine("Es palíndroma");
+            else Console.WriteLine("No es palíndroma");
+            Console.ReadLine();
+        }
+
+        static Boolean esPalindroma(String cadena)
+        {
+            Console.WriteLine("Función llamada con => " + cadena);
+            if (cadena.Length < 2) return true;
+            Console.WriteLine("Vamos a comparar => " + cadena[0]);
+            Console.WriteLine("Con => " + cadena[cadena.Length - 1]);
+            if (cadena[0] == cadena[cadena.Length - 1]) return esPalindroma(cadena.Substring(1, cadena.Length - 2));
+            return false;
+        }
+    }
+}
+
