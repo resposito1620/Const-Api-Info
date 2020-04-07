4e35584627f09767de8a8b66c4286c5ab8464c42
resposito1620 <62219245+resposito1620@users.noreply.github.com>
2020-04-07 19:20:19 -0300

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nroIngresado = "";
+            int comprobacion = 0;
+            string cadenaInvertida = "";
+            do
+            {
+                Console.WriteLine("Ingrese un número:");
+                nroIngresado = Console.ReadLine();
+                if (!int.TryParse(nroIngresado, out comprobacion))
+                {
+                    Console.WriteLine("Error. No se ingresó un número.");
+                }
+            } while (!int.TryParse(nroIngresado, out comprobacion));
+            foreach (char letra in nroIngresado)
+            {
+                cadenaInvertida = letra + cadenaInvertida;
+            }
+            Console.WriteLine("El número invertido es: " + cadenaInvertida);
+            Console.WriteLine("Presione una tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
