b91f57aee37f1b719a7c634030789778b98ba1c1
resposito1620 <62219245+resposito1620@users.noreply.github.com>
2020-04-07 19:22:54 -0300

    Add files via upload

---
+++ Ejercicio9/Ejercicio9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            int comprobacion = 0;
+            do
+            {
+                Console.WriteLine("Ingrese su nombre, por favor:");
+                nombre = Console.ReadLine();
+                if (!int.TryParse(nombre, out comprobacion))
+                {
+                    Console.WriteLine("¡Hola " + nombre + "!");
+                }
+                else
+                {
+                    Console.WriteLine("No te conozco.");
+                }
+            } while (int.TryParse(nombre, out comprobacion));
+            Console.WriteLine("Presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
