17c362128173e2fa0a95abee69cc8fb994d6dcd5
resposito1620 <62219245+resposito1620@users.noreply.github.com>
2020-04-07 19:23:44 -0300

    Add files via upload

---
+++ Ejercicio10/Ejercicio10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "";
+            string numero = "";
+            int edad = 0;
+            string nombre = "";
+            string apellido = "";
+
+            Console.WriteLine("Ingrese nombre, apellido, y edad:");
+            cadena = Console.ReadLine();
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (char.IsDigit(cadena[i]))
+                {
+                    numero += cadena[i];
+                }
+            }
+            if (numero.Length > 0)
+            {
+                edad = int.Parse(numero);
+            }
+
+            string cadenaSinNro = cadena.Trim(new Char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ' });
+
+            String[] palabras = cadenaSinNro.Split(' ');
+            nombre = palabras[0];
+            apellido = palabras[1];
+
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Apellido: " + apellido);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Presione cualquier tecla para salir.");
+
+            Console.ReadKey();
+        }
+    }
+}
