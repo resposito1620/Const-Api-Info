60f6d1ca670213644e2bcc9d0fcad1b4acbb420f
resposito1620 <62219245+resposito1620@users.noreply.github.com>
2020-06-01 03:26:07 -0300

    Add files via upload

---
+++ AyudaConsola.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGeneral
+{
+    public static class AyudaConsola
+    {
+        public static double PedirDouble(string msg)
+        {
+            Console.WriteLine("Ingrese " + msg);
+            double c = Validaciones.ValidarDouble(Console.ReadLine());
+            if (c < 0)
+            {
+                throw new Exception("El valor ingresado no es válido.");
+            }
+            else
+            {
+                return c;
+            }
+        }
+        public static bool EsOpcionValida(string input, string opcionesValidas)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(input) || input.Length > 1 || !opcionesValidas.ToUpper().Contains(input.ToUpper()))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Validaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGeneral
+{
+    public static class Validaciones
+    {
+        public static double ValidarDouble(string numero)
+        {
+            double comprobacion = 0;
+            if (!double.TryParse(numero, out comprobacion))
+            {
+                return -1;
+            }
+            else
+            {
+                return comprobacion;
+            }
+        }
+    }
+}
